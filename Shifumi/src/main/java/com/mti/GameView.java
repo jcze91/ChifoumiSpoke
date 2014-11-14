6a946ca93c0fe07f96a14582b09e084e3782835a
Julien <jcze91@gmail.com>
2014-11-14 18:22:16 +0100

    gameview

--- Shifumi/src/main/java/com/mti/GameView.java
+++ Shifumi/src/main/java/com/mti/GameView.java
@@ -2,84 +2,83 @@ package com.mti;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.*;
-import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * Created by JULIEN on 11/14/2014.
  */
 public class GameView extends VerticalLayout implements View {
     private final Label youLabel = new Label();
+    private final Label youScore = new Label();
+    private final Label iaScore = new Label();
 
     public GameView() {
         setSizeFull();
-        Panel panel = new Panel();
-        VerticalLayout panelContent = new VerticalLayout();
+        Panel topPanel = new Panel();
+        Panel bodyPanel = new Panel();
+        topPanel.setHeight(50, Unit.PIXELS);
+        VerticalLayout content = new VerticalLayout();
+        HorizontalLayout topContent = new HorizontalLayout();
+        HorizontalLayout bodyContent = new HorizontalLayout();
+
+        HorizontalLayout youContent = new HorizontalLayout();
+        HorizontalLayout iaContent = new HorizontalLayout();
+        iaContent.setWidth(100, Unit.PIXELS);
+        youContent.setWidth(100, Unit.PIXELS);
+
         Label iaLabel = new Label("IA");
 
-        panelContent.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-        panelContent.addComponent(youLabel);
-        panelContent.addComponent(iaLabel);
+        youContent.addComponent(youLabel);
+        youContent.addComponent(youScore);
+        iaContent.addComponent(iaLabel);
+        iaContent.addComponent(iaScore);
+        topContent.addComponent(youContent);
+        topContent.addComponent(iaContent);
+        topContent.setComponentAlignment(youContent, Alignment.MIDDLE_LEFT);
+        topContent.setComponentAlignment(iaContent, Alignment.MIDDLE_RIGHT);
 
-        Button paperButton = new Button("Paper",
+        bodyContent.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        Button rock = new Button("ROCK",
                 new Button.ClickListener() {
                     @Override
                     public void buttonClick(Button.ClickEvent event) {
-                        Notification.show("Click");
                     }
                 });
-        paperButton.setStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        paperButton.setIcon(new ThemeResource("~/pizza.jpg"));
-        panelContent.addComponent(paperButton);
-
-        Button rockButton = new Button("Rock",
+        Button paper = new Button("PAPER",
                 new Button.ClickListener() {
                     @Override
                     public void buttonClick(Button.ClickEvent event) {
-                        Notification.show("Click");
                     }
                 });
-        rockButton.setStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        rockButton.setIcon(new ThemeResource("~/pizza.jpg"));
-        panelContent.addComponent(rockButton);
-
-        Button scissorsButton = new Button("Rock",
+        Button scissors = new Button("SCISSORS",
                 new Button.ClickListener() {
                     @Override
                     public void buttonClick(Button.ClickEvent event) {
-                        Notification.show("Click");
                     }
                 });
-        scissorsButton.setStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        scissorsButton.setIcon(new ThemeResource("~/pizza.jpg"));
-        panelContent.addComponent(scissorsButton);
-
-        Button spokeButton = new Button("Spoke",
+        Button spoke = new Button("SPOKE",
                 new Button.ClickListener() {
                     @Override
                     public void buttonClick(Button.ClickEvent event) {
-                        Notification.show("Click");
                     }
                 });
-        spokeButton.setStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        spokeButton.setIcon(new ThemeResource("~/pizza.jpg"));
-        panelContent.addComponent(spokeButton);
-
-        Button lizardButton = new Button("Lizard",
+        Button lizard = new Button("LIZARD",
                 new Button.ClickListener() {
                     @Override
                     public void buttonClick(Button.ClickEvent event) {
-                        Notification.show("Click");
                     }
                 });
-        lizardButton.setStyleName(ValoTheme.BUTTON_ICON_ONLY);
-        lizardButton.setIcon(new ThemeResource("~/pizza.jpg"));
-        panelContent.addComponent(lizardButton);
 
-        panel.setContent(panelContent);
-        addComponent(panel);
-        setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
+        bodyContent.addComponents(rock, paper, scissors, spoke, lizard);
+
+        topPanel.setContent(topContent);
+        bodyPanel.setContent(bodyContent);
+
+        setDefaultComponentAlignment(Alignment.TOP_CENTER);
+
+        addComponent(topPanel);
+        addComponent(bodyPanel);
     }
 
     @Override
