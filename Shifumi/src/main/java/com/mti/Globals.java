449c4e904ddf8b6ee3c994749b97bf7d52636436
Julien <jcze91@gmail.com>
2014-11-15 11:50:37 +0100

    fix

--- Shifumi/src/main/java/com/mti/Globals.java
+++ Shifumi/src/main/java/com/mti/Globals.java
@@ -4,10 +4,6 @@ package com.mti;
  * Created by JULIEN on 11/14/2014.
  */
 public class Globals {
-    public static final String VIEW_START = "start";
-    public static final String VIEW_LOBBY = "lobby";
-    public static final String VIEW_GAME = "game";
-
     public static final String SESSION_USERNAME = "username";
 
 }
